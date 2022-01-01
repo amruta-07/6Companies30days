5a78eb07574337377f33826b85f4b3d8fbc7f342
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-01 22:46:57 +0530

    Add files via upload

---
+++ Milestone-1/Goldman Sachs/Day-1/SubArray.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long prod=1;
+       int j=0,ans=0;
+       for(int i=0;i<n;i++){
+           prod*=a[i];
+           while(j<n&&prod>=k){
+               prod/=a[j];
+               j++;
+           }
+           ans+=(i-j+1);
+       }
+       return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends

96cf5904c303ed1705761ea97aa207e33f82b487
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-02 21:46:09 +0530

    Add files via upload

--- Milestone-1/Goldman Sachs/Day-2/RunEncoding.java
+++ Milestone-1/Goldman Sachs/Day-2/RunEncoding.java
@@ -1,1 +1,38 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
 
+string encode(string src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string str;
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
+}// } Driver Code Ends
+
+
+/*You are required to complete this function */
+
+string encode(string s)
+{     
+  //Your code here 
+   string ans="";
+   int i=0,j=0;
+   while(j<s.length()){
+       while(s[i]==s[j]){
+           j++;
+       }
+       ans+=s[i] + to_string(j-i);
+       i=j;
+   }
+   return ans;
+}     
+ 

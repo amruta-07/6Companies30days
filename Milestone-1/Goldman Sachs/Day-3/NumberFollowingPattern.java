c2d15dd456f113e527cf639a35f823686363cfb8
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-03 20:04:00 +0530

    Add files via upload

--- Milestone-1/Goldman Sachs/Day-3/NumberFollowingPattern.java
+++ Milestone-1/Goldman Sachs/Day-3/NumberFollowingPattern.java
@@ -1,1 +1,92 @@
+Number following a pattern
 
+Example 1:
+
+Input:
+D
+Output:
+21
+Explanation:
+D is meant for decreasing,
+So we choose the minimum number
+among 21,31,54,87,etc.
+ 
+
+Example 2:
+
+Input:
+IIDDD
+Output:
+126543
+Explanation:
+Above example is self- explanatory,
+1 < 2 < 6 > 5 > 4 > 3
+  I - I - D - D - D
+ 
+
+ 
+
+Your Task:
+
+You don't need to read input or print anything. Your task is to complete the function printMinNumberForPattern() which takes the string S and returns a string containing the minimum number following the valid pattern.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+ 
+
+Constraints:
+1 ≤ Length of String ≤ 8
+
+
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution{   
+public:
+        string printMinNumberForPattern(string s){
+        string ans;
+        stack<int> st;
+        int num = 1;
+        for(auto it:s){
+            if(it == 'D'){
+                st.push(num);
+                num++;
+            }else{
+                st.push(num);
+                num++;
+                while(!st.empty()){
+                    ans += to_string(st.top());
+                    st.pop();
+                }
+            }
+        }
+        st.push(num);
+        while(!st.empty()){
+            ans += to_string(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.printMinNumberForPattern(S) << endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends

e164f9629d207b22312a2e718b88f81d219a7d9b
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-04 23:22:26 +0530

    Add files via upload

--- Milestone-1/Goldman Sachs/Day-4/Chessboard.java
+++ Milestone-1/Goldman Sachs/Day-4/Chessboard.java
@@ -1,1 +1,33 @@
+Squares in N*N Chessboard
 
+
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+       // code here
+       return (n*(n+1)*((2*n)+1))/6;
+   }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends

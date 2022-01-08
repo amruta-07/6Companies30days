9af2f1c297118d8c1b8bb1765ebdea9bac207f25
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-08 23:21:47 +0530

    Add files via upload

---
+++ Milestone-2/Amazon/Day-3/NonRepeating.cpp
@@ -0,0 +1,46 @@
+First non-repeating character in a stream
+
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		string FirstNonRepeating(string str){
+        string ans = "";
+        deque<char> dq;
+        int hash[26]={0};
+        
+        for(auto it:str){
+            int pos = it - 'a';
+            
+            if(hash[pos] == 0){
+                dq.push_back(it);
+            } 
+            hash[pos]++;
+            
+            while(!dq.empty() and hash[dq.front() - 'a'] != 1) dq.pop_front();
+            
+            if(dq.empty()) ans += '#';
+            else ans += dq.front();   
+        }
+        return ans;
+	}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends

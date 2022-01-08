a9fb3ab50163aa481e55b922cdf4f35ce80b1984
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-08 23:21:17 +0530

    Add files via upload

---
+++ Milestone-2/Amazon/Day-3/CountN.cpp
@@ -0,0 +1,38 @@
+Count ways to N'th Stair(Order does not matter)
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int m)
+    {
+        long long ans=m/2+1;
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends

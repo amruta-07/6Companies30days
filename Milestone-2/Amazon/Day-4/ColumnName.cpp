3e12c35f45285765180711a22db1edcf5051013d
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-09 23:26:55 +0530

    Add files via upload

--- Milestone-2/Amazon/Day-4/ColumnName.cpp
+++ Milestone-2/Amazon/Day-4/ColumnName.cpp
@@ -1,1 +1,36 @@
+Column name from a given column number
 
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    string colName (long long int n) {
+        string s = "";
+        while(n != 0) {
+            // To make the number 0 indexed
+            n--;
+            int r = n % 26;
+            s = string(1, r + 'A') + s;
+            n = n / 26;
+        }
+        return s;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+  // } Driver Code Ends

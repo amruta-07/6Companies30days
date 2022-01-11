01cdbabc4a8ed7d7cbf327620743b730b0ffce58
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-11 22:46:57 +0530

    Add files via upload

--- Milestone-3/Microsoft/Day-1/Rotate.java
+++ Milestone-3/Microsoft/Day-1/Rotate.java
@@ -1,1 +1,73 @@
+Rotate By 90 degree
 
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*; 
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+	public static void main(String[] args) 
+	{ 
+	    Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[][] arr = new int[n][n];
+            
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    arr[i][j] = sc.nextInt();
+            
+            GFG g = new GFG();
+            g.rotate(arr);
+            printMatrix (arr);
+        }
+	} 
+	
+	static void printMatrix(int arr[][]) 
+	{ 
+		for (int i = 0; i < arr.length; i++) { 
+			for (int j = 0; j < arr[0].length; j++) 
+				System.out.print(arr[i][j] + " "); 
+			System.out.println(""); 
+		} 
+	} 
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class GFG
+{
+   static void rotate(int matrix[][]) 
+   {
+       // Code Here
+       int n=matrix.length;
+       int m=matrix[0].length;
+       //transpose of matrix
+       for(int i=0;i<n;i++)
+       {
+           for(int j=i+1;j<m;j++)
+           {
+               int tmp=matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=tmp;
+           }
+       }
+       //reversing rows
+       for(int i=0;i<n/2;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               int tmp=matrix[i][j];
+               matrix[i][j]=matrix[m-i-1][j];
+               matrix[m-i-1][j]=tmp;
+           }
+       }
+   }
+}

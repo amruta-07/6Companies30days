70dea20d058899880f3bf25b9963a904b452a39a
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-12 22:48:29 +0530

    Add files via upload

--- Milestone-3/Microsoft/Day-2/StockSpan.java
+++ Milestone-3/Microsoft/Day-2/StockSpan.java
@@ -1,1 +1,60 @@
+Stock span problem
 
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int a[] = new int[n];
+            
+            int i = 0;
+            for(i = 0; i < n; i++)
+              a[i] = sc.nextInt();
+              
+            int[] s = new Solution().calculateSpan(a, n);
+            
+            for(i = 0; i < n; i++)
+            {
+                System.out.print(s[i] + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    
+    
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to calculate the span of stockâ€™s price for all n days.
+    public static int[] calculateSpan(int price[], int n)
+    {
+        int [] result = new int[n];
+        result[0] = 1;
+        for (int i=1; i<n; i++) {
+            result[i] = 1;
+            for (int j = i-1; j>=0; j -= result[j]) {
+                if (price[j] > price[i]) 
+                    break;  
+                result[i] += result[j];
+            }
+        }
+        return result;
+    }
+    
+}

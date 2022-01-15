46dd356fbee7ee5e5be3bcd45788d2c738dfa7df
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2022-01-15 23:59:50 +0530

    Add files via upload

--- Milestone-3/Microsoft/Day-5/AddDigits.java
+++ Milestone-3/Microsoft/Day-5/AddDigits.java
@@ -1,1 +1,19 @@
+258. Add Digits   //Leetcode Question 
 
+
+
+class Solution {
+    public int addDigits(int num) {
+        int digitalRoot = 0;
+        while (num > 0) {
+            digitalRoot += num % 10;
+            num = num / 10;
+            
+            if (num == 0 && digitalRoot > 9) {
+                num = digitalRoot;
+                digitalRoot = 0;  
+            }    
+        }     
+        return digitalRoot;
+    }
+}
